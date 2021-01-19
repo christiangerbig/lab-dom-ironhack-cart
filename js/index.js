08c86efcd679f28e009b617dbce1f826605024bc
christiangerbig <chr_gerbig@web.de>
2021-01-19 20:35:13 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,31 +1,38 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const priceSelector = product.querySelector(".price span");
+  const quantitySelector = product.querySelector(".quantity input");
+  let price = Number(priceSelector.innerText);
+  let quantity = (quantitySelector.value);
+  let subtotal = price * quantity;
+  const subtotalSelector = product.querySelector(".subtotal span");
+  subtotalSelector.innerText = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let productsSelectors = document.getElementsByClassName("product");
+  let totalSum = 0;
+  for (let singleProduct of productsSelectors) {
+    totalSum += updateSubtotal(singleProduct);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  let = totalSumSelector = document.querySelector("#total-value span");
+  totalSumSelector.innerText = totalSum;
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  let parent = target.parentNode;
+  parent.removeChild(target);
+ 
 }
 
 // ITERATION 5
@@ -38,5 +45,9 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtnSelectors = document.getElementsByClassName("btn-remove");
+  for (let removeBtnSelector of removeBtnSelectors) {
+    removeBtnSelector.addEventListener('click', removeProduct);
+  }
 });
+  
